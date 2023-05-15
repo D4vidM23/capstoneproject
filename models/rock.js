0c3f335a8874c4fde54466c8def8277863752902
zxtt <zxtt@fresnou.org>
2023-05-15 11:20:52 -0700

    add this too

---
+++ models/rock.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Rock extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Rock.init({
+    seller: DataTypes.STRING,
+    product: DataTypes.STRING,
+    image: DataTypes.STRING,
+    description: DataTypes.STRING,
+    price: DataTypes.STRING,
+  }, {
+    sequelize,
+    modelName: 'Rock',
+    timestamps: false,
+    tableName: 'rock_sells'
+  });
+  return Rock;
+};
+
